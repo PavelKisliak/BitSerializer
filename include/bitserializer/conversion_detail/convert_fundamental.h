abe5214ebb6a19fd0adcb567a169bfa5e64cf85c
Pavel_Kisliak <Pavel_Kisliak@epam.com>
2018-03-14 14:12:36 +0300

    A little optimizations in conversions of integral types.

--- include/bitserializer/conversion_detail/convert_fundamental.h
+++ include/bitserializer/conversion_detail/convert_fundamental.h
@@ -20,23 +20,27 @@ template <typename T> inline void ToString(T val, std::wstring& ret_Str)		{ ret_
 template <class T, typename TSym, typename TAllocator, std::enable_if_t<(std::is_unsigned_v<T>), int> = 0>
 inline void FromString(const std::basic_string<TSym, std::char_traits<TSym>, TAllocator>& str, T& ret_Val)
 {
-	auto result = std::stoull(str, 0, 0);
+	auto result = std::stoul(str, 0, 0);
 	if (result < std::numeric_limits<T>::min() || std::numeric_limits<T>::max() < result) {
 		throw std::out_of_range("argument out of range");
 	}
 	ret_Val = static_cast<T>(result);
 }
+inline void FromString(const std::string& str, unsigned long long& ret_Val)		{ ret_Val = std::stoull(str, 0, 0); }
+inline void FromString(const std::wstring& str, unsigned long long& ret_Val)	{ ret_Val = std::stoull(str, 0, 0); }
 
 // Convert from signed decimal to string
 template <class T, typename TSym, typename TAllocator, std::enable_if_t<(std::is_signed_v<T>), int> = 0>
 inline void FromString(const std::basic_string<TSym, std::char_traits<TSym>, TAllocator>& str, T& ret_Val)
 {
-	auto result = std::stoll(str, 0, 0);
+	auto result = std::stol(str, 0, 0);
 	if (result < std::numeric_limits<T>::min() || std::numeric_limits<T>::max() < result) {
 		throw std::out_of_range("argument out of range");
 	}
 	ret_Val = static_cast<T>(result);
 }
+inline void FromString(const std::string& str, long long& ret_Val)				{ ret_Val = std::stoll(str, 0, 0); }
+inline void FromString(const std::wstring& str, long long& ret_Val)				{ ret_Val = std::stoll(str, 0, 0); }
 
 // Convert from string to boolean
 inline void FromString(const std::string& str, bool& ret_Val)					{ ret_Val = std::stoi(str) ? true : false; }

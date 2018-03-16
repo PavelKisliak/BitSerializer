7b827196f4ae9d7c1e01e682bfa177040e803b7c
Pavel_Kisliak <Pavel_Kisliak@epam.com>
2018-03-16 12:26:48 +0300

    Add public method for get map of enum descriptors, add macros for simplify registration.

--- include/bitserializer/conversion_detail/convert_enum.h
+++ include/bitserializer/conversion_detail/convert_enum.h
@@ -8,26 +8,80 @@
 #include <cctype>
 #include <algorithm>
 
+/// <summary>
+/// Registers a map of strings equivalents for enum type.
+/// </summary>
+/// <example>
+/// REGISTER_ENUM_MAP(YOUR_ENUM_TYPE)
+///	{
+///		{ YOUR_ENUM_TYPE::Apple, "Apple" },
+///		{ YOUR_ENUM_TYPE::Orange, "Orange" }
+/// }
+/// END_ENUM_MAP()
+/// </example>
+#define REGISTER_ENUM_MAP(enumType) namespace BitSerializer::Convert::Detail { \
+	static const bool registration_##enumType = ConvertEnum::Register<enumType>(
+#define END_ENUM_MAP() ); }
+
 namespace BitSerializer::Convert::Detail {
 
-///------------------------------------------------------------------------------
-/// Implementation detail for convert enumeration types
-///
-/// For able to convert your enum-types to strings and vice versa you need to register it:
-/// static const bool _YOUR_ENUM = ConvertEnum::Register<YOUR_ENUM>(
-///	{
-///		{ YOUR_ENUM::Apple, "Apple" },
-///		{ YOUR_ENUM::Orange, "Orange" },
-///		...
-///	};
-///------------------------------------------------------------------------------
+template <typename TEnum>
+class EnumDescriptor
+{
+public:
+	EnumDescriptor() = default;
+	EnumDescriptor(TEnum value, const char* name)
+		: mValue(value)
+		, mStrName(name)
+	{ }
+
+	template <typename TSym, typename TAllocator>
+	inline bool Equals(const std::basic_string<TSym, std::char_traits<TSym>, TAllocator>& str_value) const
+	{
+		return std::equal(str_value.cbegin(), str_value.cend(), mStrName.cbegin(), mStrName.cend(), [](const TSym lhs, const char rhs) {
+			return std::tolower(static_cast<char>(lhs)) == std::tolower(static_cast<char>(rhs));
+		});
+	}
+
+	inline bool Equals(TEnum enum_value) const noexcept {
+		return enum_value == mValue;
+	}
+
+	template <typename TSym, typename TAllocator>
+	inline void GetName(std::basic_string<TSym, std::char_traits<TSym>, TAllocator>& ret_Str) const
+	{
+		ret_Str.clear();
+		ret_Str.append(mStrName.begin(), mStrName.end());
+	}
+
+	inline TEnum GetEnum() const noexcept {
+		return mValue;
+	}
+
+private:
+	TEnum mValue;
+	std::string mStrName;
+};
+
 class ConvertEnum
 {
 public:
 	template <typename TEnum>
+	using enum_descriptors = std::vector<EnumDescriptor<TEnum>>;
+
+	template <typename TEnum>
+	static const enum_descriptors<TEnum>& GetDescriptors() noexcept
+	{
+		auto& descriptors = GetDescriptorsImpl<TEnum>();
+		// Make sure, that type is registered
+		assert(!descriptors.empty());
+		return const_cast<enum_descriptors<TEnum>&>(descriptors);
+	}
+
+	template <typename TEnum>
 	static bool Register(const std::initializer_list<std::pair<TEnum, const char*>> descriptors)
 	{
-		auto& staticDescriptors = EnumValueDescriptor<TEnum>::GetStaticDescriptors();
+		auto& staticDescriptors = GetDescriptorsImpl<TEnum>();
 		// Check is that type was already registered
 		if (!staticDescriptors.empty())
 			return true;
@@ -42,11 +96,8 @@ public:
 	template <typename TEnum, typename TSym, typename TAllocator>
 	static bool ToString(TEnum val, std::basic_string<TSym, std::char_traits<TSym>, TAllocator>& ret_Str)
 	{
-		auto descriptors = EnumValueDescriptor<TEnum>::GetStaticDescriptors();
-		// Make sure, that type is registered
-		assert(!descriptors.empty());
-
-		auto it = std::find_if(descriptors.cbegin(), descriptors.cend(), [val](const EnumValueDescriptor<TEnum>& descr) {
+		const auto& descriptors = GetDescriptors<TEnum>();
+		auto it = std::find_if(descriptors.cbegin(), descriptors.cend(), [val](const EnumDescriptor<TEnum>& descr) {
 			return descr.Equals(val);
 		});
 		if (it == descriptors.end())
@@ -59,11 +110,8 @@ public:
 	template <typename TEnum, typename TSym, typename TAllocator>
 	static bool FromString(const std::basic_string<TSym, std::char_traits<TSym>, TAllocator>& str, TEnum& ret_Val)
 	{
-		auto descriptors = EnumValueDescriptor<TEnum>::GetStaticDescriptors();
-		// Make sure, that type is registered
-		assert(!descriptors.empty());
-
-		auto it = std::find_if(descriptors.cbegin(), descriptors.cend(), [&str](const EnumValueDescriptor<TEnum>& descr) {
+		const auto& descriptors = GetDescriptors<TEnum>();
+		auto it = std::find_if(descriptors.cbegin(), descriptors.cend(), [&str](const EnumDescriptor<TEnum>& descr) {
 			return descr.Equals(str);
 		});
 		if (it == descriptors.end())
@@ -75,46 +123,11 @@ public:
 
 private:
 	template <typename TEnum>
-	class EnumValueDescriptor
+	static enum_descriptors<TEnum>& GetDescriptorsImpl() noexcept
 	{
-	public:
-		EnumValueDescriptor() = default;
-		EnumValueDescriptor(TEnum value, const char* name)
-			: mValue(value)
-			, mStrName(name)
-		{ }
-
-		static std::vector<EnumValueDescriptor<TEnum>>& GetStaticDescriptors() noexcept
-		{
-			static std::vector<EnumValueDescriptor<TEnum>> descriptors;
-			return descriptors;
-		}
-
-		template <typename TSym, typename TAllocator>
-		inline bool Equals(const std::basic_string<TSym, std::char_traits<TSym>, TAllocator>& str_value) const {
-			return std::equal(str_value.cbegin(), str_value.cend(), mStrName.cbegin(), mStrName.cend(), [](const TSym lhs, const char rhs) {
-				return std::tolower(static_cast<char>(lhs)) == std::tolower(static_cast<char>(rhs));
-			});
-		}
-
-		inline bool Equals(TEnum enum_value) const noexcept {
-			return enum_value == mValue;
-		}
-
-		template <typename TSym, typename TAllocator>
-		inline void GetName(std::basic_string<TSym, std::char_traits<TSym>, TAllocator>& ret_Str) const {
-			ret_Str.clear();
-			ret_Str.append(mStrName.begin(), mStrName.end());
-		}
-
-		inline TEnum GetEnum() const noexcept {
-			return mValue;
-		}
-
-	private:
-		TEnum mValue;
-		std::string mStrName;
-	};
+		static enum_descriptors<TEnum> descriptors;
+		return descriptors;
+	}
 };
 
 }	// namespace BitSerializer::Convert::Detail

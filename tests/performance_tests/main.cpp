d0638a2cba7c6f3796f9db371e3160355a3cc300
Pavel_Kisliak <real@tut.by>
2021-05-06 21:44:35 +0300

    Update performance test - execution will be based on time rather than number of retries

--- tests/performance_tests/main.cpp
+++ tests/performance_tests/main.cpp
@@ -1,5 +1,5 @@
 /*******************************************************************************
-* Copyright (C) 2020 by Pavel Kisliak                                          *
+* Copyright (C) 2018-2021 by Pavel Kisliak                                     *
 * This file is part of BitSerializer library, licensed under the MIT license.  *
 *******************************************************************************/
 #include <cassert>
@@ -12,13 +12,15 @@
 #include "pugixml_performance_test.h"
 #include "rapid_yaml_perfomance_test.h"
 
+constexpr auto DefaultArchiveTestTimeSec = 60;
 
 using Timer = std::chrono::high_resolution_clock;
 
 struct TestArchiveMetadata
 {
 	TestArchiveMetadata() = default;
-	TestArchiveMetadata(const char* name) : mName(name), mMinSaveTime(0), mMinLoadTime(0), mMinNativeSaveTime(0), mMinNativeLoadTime(0) {}
+	explicit TestArchiveMetadata(const char* name)
+		: mName(name), mMinSaveTime(0), mMinLoadTime(0), mMinNativeSaveTime(0), mMinNativeLoadTime(0) {}
 
 	const char* mName;
 	std::chrono::nanoseconds mMinSaveTime;
@@ -27,16 +29,22 @@ struct TestArchiveMetadata
 	std::chrono::nanoseconds mMinNativeLoadTime;
 };
 
-template <class TArchive, class TPerformanceTestModel, int TestCycles>
+template <class TArchive, class TPerformanceTestModel, int TestTimeSec = DefaultArchiveTestTimeSec>
 TestArchiveMetadata TestArchivePerformance()
 {
 	auto origModel = BuildFixture<TPerformanceTestModel>();
 	TestArchiveMetadata metadata(origModel.GetName());
 
 	int percent = -1;
-	for (int i = 0; i < TestCycles; i++)
+	bool needVerify = true;
+
+	const auto beginTime = Timer::now();
+	const auto endTime = beginTime + std::chrono::seconds(TestTimeSec);
+	const auto testTimeMSec = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::seconds(TestTimeSec)).count();
+
+	for (auto time = beginTime; time < endTime; time = Timer::now())
 	{
-		const auto newPercent = static_cast<int>(i / (TestCycles * 0.01));
+		const auto newPercent = static_cast<int>(std::chrono::duration_cast<std::chrono::milliseconds>(time - beginTime).count() * 100 / testTimeMSec);
 		if (percent != newPercent)
 		{
 			percent = newPercent;
@@ -63,7 +71,7 @@ TestArchiveMetadata TestArchivePerformance()
 		if (metadata.mMinNativeSaveTime == std::chrono::nanoseconds(0) || metadata.mMinNativeSaveTime > nativeSaveTime) metadata.mMinNativeSaveTime = nativeSaveTime;
 
 		// Check an identity of two results (for test saver via native code)
-		if (i == 0)
+		if (needVerify)
 		{
 			using NativeOutputType = decltype(origModel.TestSave());
 			if constexpr (std::is_same_v<NativeOutputType, typename TArchive::preferred_output_format>) {
@@ -82,8 +90,11 @@ TestArchiveMetadata TestArchivePerformance()
 		if (metadata.mMinNativeLoadTime == std::chrono::nanoseconds(0) || metadata.mMinNativeLoadTime > nativeLoadTime) metadata.mMinNativeLoadTime = nativeLoadTime;
 
 		// Check integrity (for test loader via native code)
-		if (i == 0)
+		if (needVerify)
+		{
+			needVerify = false;
 			testModelForNativeCode.Assert(origModel);
+		}
 	}
 
 	std::cout << "\r";
@@ -110,10 +121,10 @@ int main()
 	{
 		std::cout << "Testing, please do not touch mouse and keyboard (test may take few minutes)." << std::endl;
 		const TestArchiveMetadata metadataList[] = {
-			  TestArchivePerformance<BitSerializer::Json::RapidJson::JsonArchive, RapidJsonPerformanceTestModel, 100000>()
-			, TestArchivePerformance<BitSerializer::Json::CppRest::JsonArchive, CppRestJsonPerformanceTestModel, 20000>()
-			, TestArchivePerformance<BitSerializer::Xml::PugiXml::XmlArchive, PugiXmlPerformanceTestModel, 50000>()
-			, TestArchivePerformance<BitSerializer::Yaml::RapidYaml::YamlArchive, RapidYamlPerformanceTestModel, 20000>()
+			  TestArchivePerformance<BitSerializer::Json::RapidJson::JsonArchive, RapidJsonPerformanceTestModel>()
+			, TestArchivePerformance<BitSerializer::Json::CppRest::JsonArchive, CppRestJsonPerformanceTestModel>()
+			, TestArchivePerformance<BitSerializer::Xml::PugiXml::XmlArchive, PugiXmlPerformanceTestModel>()
+			, TestArchivePerformance<BitSerializer::Yaml::RapidYaml::YamlArchive, RapidYamlPerformanceTestModel>()
 		};
 	}
 	catch (const std::exception& ex) {

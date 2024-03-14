b7dc1bbcaff8ddf8f941dc34c25f2bf80a4f8769
Pavel Kisliak <pavel_kisliak@epam.com>
2024-03-14 21:52:36 +0300

    [Core] Add support for serialization std::filesystem::path

--- tests/bitserializer_conversions_tests/convert_filesystem_tests.cpp
+++ tests/bitserializer_conversions_tests/convert_filesystem_tests.cpp
@@ -14,51 +14,51 @@ using namespace BitSerializer;
 //-----------------------------------------------------------------------------
 // Test conversion for filesystem::path
 //-----------------------------------------------------------------------------
-TEST(ConvertStd, ConvertAnsiPathToAnyString) {
+TEST(ConvertFilesystem, ConvertAnsiPathToAnyString) {
 	EXPECT_EQ("c:/temp", Convert::ToString(std::filesystem::path("c:/temp")));
 	EXPECT_EQ(u"c:/temp", Convert::To<std::u16string>(std::filesystem::path(u"c:/temp")));
 	EXPECT_EQ(L"c:/temp", Convert::ToWString(std::filesystem::path(L"c:/temp")));
 	EXPECT_EQ(U"c:/temp", Convert::To<std::u32string>(std::filesystem::path(U"c:/temp")));
 }
 
-TEST(ConvertStd, ConvertNativePathToUtf8) {
+TEST(ConvertFilesystem, ConvertNativePathToUtf8) {
 	EXPECT_EQ(UTF8("c:/Привет Мир"), Convert::ToString(std::filesystem::path(UPATH("c:/Привет Мир"))));
 	EXPECT_EQ(UTF8("c:/世界，您好"), Convert::ToString(std::filesystem::path(UPATH("c:/世界，您好"))));
 }
 
-TEST(ConvertStd, ConvertNativePathToWString) {
+TEST(ConvertFilesystem, ConvertNativePathToWString) {
 	EXPECT_EQ(L"c:/Привет Мир", Convert::ToWString(std::filesystem::path(UPATH("c:/Привет Мир"))));
 	EXPECT_EQ(L"c:/世界，您好", Convert::ToWString(std::filesystem::path(UPATH("c:/世界，您好"))));
 }
 
-TEST(ConvertStd, ConvertNativePathToUtf16) {
+TEST(ConvertFilesystem, ConvertNativePathToUtf16) {
 	EXPECT_EQ(u"c:/Привет Мир", Convert::To<std::u16string>(std::filesystem::path(UPATH("c:/Привет Мир"))));
 	EXPECT_EQ(u"c:/世界，您好", Convert::To<std::u16string>(std::filesystem::path(UPATH("c:/世界，您好"))));
 }
 
-TEST(ConvertStd, ConvertNativePathToUtf32) {
+TEST(ConvertFilesystem, ConvertNativePathToUtf32) {
 	EXPECT_EQ(U"c:/Привет Мир", Convert::To<std::u32string>(std::filesystem::path(UPATH("c:/Привет Мир"))));
 	EXPECT_EQ(U"c:/世界，您好", Convert::To<std::u32string>(std::filesystem::path(UPATH("c:/世界，您好"))));
 }
 
 //-----------------------------------------------------------------------------
 
-TEST(ConvertStd, ConvertUtf8StringToPath) {
+TEST(ConvertFilesystem, ConvertUtf8StringToPath) {
 	EXPECT_EQ(UPATH("c:/Привет Мир"), Convert::To<std::filesystem::path>(UTF8("c:/Привет Мир")).native());
 	EXPECT_EQ(UPATH("c:/世界，您好"), Convert::To<std::filesystem::path>(UTF8("c:/世界，您好")).native());
 }
 
-TEST(ConvertStd, ConvertWStringToPath) {
+TEST(ConvertFilesystem, ConvertWStringToPath) {
 	EXPECT_EQ(UPATH("c:/Привет Мир"), Convert::To<std::filesystem::path>(L"c:/Привет Мир").native());
 	EXPECT_EQ(UPATH("c:/世界，您好"), Convert::To<std::filesystem::path>(L"c:/世界，您好").native());
 }
 
-TEST(ConvertStd, ConvertUtf16StringToPath) {
+TEST(ConvertFilesystem, ConvertUtf16StringToPath) {
 	EXPECT_EQ(UPATH("c:/Привет Мир"), Convert::To<std::filesystem::path>(u"c:/Привет Мир").native());
 	EXPECT_EQ(UPATH("c:/世界，您好"), Convert::To<std::filesystem::path>(u"c:/世界，您好").native());
 }
 
-TEST(ConvertStd, ConvertUtf32StringToPath) {
+TEST(ConvertFilesystem, ConvertUtf32StringToPath) {
 	EXPECT_EQ(UPATH("c:/Привет Мир"), Convert::To<std::filesystem::path>(U"c:/Привет Мир").native());
 	EXPECT_EQ(UPATH("c:/世界，您好"), Convert::To<std::filesystem::path>(U"c:/世界，您好").native());
 }

90e3507a0f38dd68de72aaa2b6dc55c925899f1c
Pavel Kisliak <pavel_kisliak@epam.com>
2024-05-05 11:30:50 +0300

    [Convert] Use modern "from_chars()" for converting float types (if it available)

--- include/bitserializer/conversion_detail/convert_fundamental.h
+++ include/bitserializer/conversion_detail/convert_fundamental.h
@@ -9,7 +9,10 @@
 #include <charconv>
 #include <limits>
 #include <stdexcept>
-#include <string>
+#include "bitserializer/config.h"
+#if BITSERIALIZER_HAS_FLOAT_FROM_CHARS == 0
+#include "convert_compatibility.h"
+#endif
 #include "convert_utf.h"
 
 namespace BitSerializer::Convert::Detail
@@ -17,7 +20,7 @@ namespace BitSerializer::Convert::Detail
 	/// <summary>
 	/// Converts any of fundamental types to any other fundamental type.
 	/// </summary>
-	template <typename TSource, typename TTarget, std::enable_if_t<std::is_arithmetic_v<TSource>&& std::is_arithmetic_v<TTarget>, int> = 0>
+	template <typename TSource, typename TTarget, std::enable_if_t<std::is_arithmetic_v<TSource> && std::is_arithmetic_v<TTarget>, int> = 0>
 	void To(const TSource& sourceValue, TTarget& targetValue)
 	{
 		if constexpr (std::is_same_v<TSource, TTarget>)
@@ -66,9 +69,13 @@ namespace BitSerializer::Convert::Detail
 	}
 
 	/// <summary>
-	/// Converts any UTF string to integer types.
+	/// Converts any UTF string to integer or floating types (except compatibility mode).
 	/// </summary>
-	template <typename T, typename TSym, std::enable_if_t<(std::is_integral_v<T>), int> = 0>
+	template <typename T, typename TSym, std::enable_if_t<(std::is_integral_v<T>
+#if BITSERIALIZER_HAS_FLOAT_FROM_CHARS
+	|| std::is_floating_point_v<T>
+#endif
+		), int> = 0>
 	void To(std::basic_string_view<TSym> in, T& out)
 	{
 		const auto* it = in.data();
@@ -91,9 +98,12 @@ namespace BitSerializer::Convert::Detail
 			}
 
 			// Check that string does not contain decimal fractions (parsing a float number to integer is not allowed)
-			if (rc.ptr + 1 < str.data() + str.size() && *rc.ptr == '.' && std::isdigit(*(rc.ptr + 1)))
+			if constexpr (std::is_integral_v<T>)
 			{
-				throw std::invalid_argument("Unable to convert string with float number to integer");
+				if (rc.ptr + 1 < str.data() + str.size() && *rc.ptr == '.' && std::isdigit(*(rc.ptr + 1)))
+				{
+					throw std::invalid_argument("Unable to convert string with float number to integer");
+				}
 			}
 		};
 
@@ -106,68 +116,6 @@ namespace BitSerializer::Convert::Detail
 			Utf8::Encode(it, end, utf8Str);
 			validateResult(std::from_chars(utf8Str.data(), utf8Str.data() + utf8Str.size(), out), utf8Str);
 		}
-	}
-
-	namespace _stdWrappers
-	{
-		template <typename T> T _fromStr(const char*, char**) { throw; }
-		template <typename T> T _fromStr(const wchar_t*, wchar_t**) { throw; }
-
-		template <>	inline float _fromStr<float>(const char* str, char** out_strEnd) { return std::strtof(str, out_strEnd); }
-		template <>	inline float _fromStr<float>(const wchar_t* str, wchar_t** out_strEnd) { return std::wcstof(str, out_strEnd); }
-
-		template <>	inline double _fromStr<double>(const char* str, char** out_strEnd) { return std::strtod(str, out_strEnd); }
-		template <>	inline double _fromStr<double>(const wchar_t* str, wchar_t** out_strEnd) { return std::wcstod(str, out_strEnd); }
-
-		template <>	inline long double _fromStr<long double>(const char* str, char** out_strEnd) { return std::strtold(str, out_strEnd); }
-		template <>	inline long double _fromStr<long double>(const wchar_t* str, wchar_t** out_strEnd) { return std::wcstold(str, out_strEnd); }
-	}
-
-	/// <summary>
-	/// Converts any UTF string to floating types.
-	/// </summary>
-	template <typename T, typename TSym, std::enable_if_t<(std::is_floating_point_v<T>), int> = 0>
-	void To(std::basic_string_view<TSym> in, T& out)
-	{
-		T result;
-		bool isNaN;
-		constexpr size_t bufSize = 64;
-		const size_t size = (in.size() < bufSize) ? in.size() : bufSize - 1;
-		if constexpr (std::is_same_v<char, TSym>)
-		{
-			// Copy to temporary buffer for prepare null-terminated c-string
-			char buf[bufSize];
-			std::memcpy(buf, in.data(), size);
-			buf[size] = 0;
-			char* endPos = nullptr;
-			result = _stdWrappers::_fromStr<T>(buf, &endPos);
-			isNaN = buf == endPos;
-		}
-		else if constexpr (sizeof(TSym) == sizeof(wchar_t))
-		{
-			wchar_t buf[bufSize];
-			std::wmemcpy(buf, reinterpret_cast<wchar_t const*>(in.data()), size);
-			buf[size] = 0;
-			wchar_t* endPos = nullptr;
-			result = _stdWrappers::_fromStr<T>(buf, &endPos);
-			isNaN = buf == endPos;
-		}
-		else
-		{
-			std::string utf8Str;
-			Utf8::Encode(in.cbegin(), in.cend(), utf8Str);
-			char* endPos = nullptr;
-			result = _stdWrappers::_fromStr<T>(utf8Str.c_str(), &endPos);
-			isNaN = utf8Str.data() == endPos;
-		}
-
-		if (result == std::numeric_limits<T>::infinity()) {
-			throw std::out_of_range("Argument out of range");
-		}
-		if (isNaN) {
-			throw std::invalid_argument("Input string is not a number");
-		}
-		out = result;
 	}
 
 	/// <summary>

5123dcb2d726fcb64fc7e675f0bfa1ac49a64c96
Pavel Kisliak <pavel_kisliak@epam.com>
2025-01-03 21:23:43 +0300

    [Core] Fix serializing `std::unordered_map` and `std::unordered_multimap` with custom allocator

--- include/bitserializer/types/std/unordered_map.h
+++ include/bitserializer/types/std/unordered_map.h
@@ -1,5 +1,5 @@
 /*******************************************************************************
-* Copyright (C) 2018-2024 by Pavel Kisliak                                     *
+* Copyright (C) 2018-2025 by Pavel Kisliak                                     *
 * This file is part of BitSerializer library, licensed under the MIT license.  *
 *******************************************************************************/
 #pragma once
@@ -11,8 +11,8 @@ namespace BitSerializer
 	/// <summary>
 	/// Serializes std::unordered_map.
 	/// </summary>
-	template<typename TArchive, typename TMapKey, typename TValue, typename TComparer, typename TAllocator>
-	void SerializeObject(TArchive& archive, std::unordered_map<TMapKey, TValue, TComparer, TAllocator>& cont, MapLoadMode mapLoadMode = MapLoadMode::Clean)
+	template<typename TArchive, typename TMapKey, typename TValue, typename THasher, typename TKeyEq, typename TAllocator>
+	void SerializeObject(TArchive& archive, std::unordered_map<TMapKey, TValue, THasher, TKeyEq, TAllocator>& cont, MapLoadMode mapLoadMode = MapLoadMode::Clean)
 	{
 		Detail::SerializeMapImpl(archive, cont, mapLoadMode);
 	}
@@ -20,8 +20,8 @@ namespace BitSerializer
 	/// <summary>
 	/// Serializes std::unordered_multimap.
 	/// </summary>
-	template<typename TArchive, typename TMapKey, typename TValue, typename TComparer, typename TAllocator>
-	void SerializeArray(TArchive& arrayScope, std::unordered_multimap<TMapKey, TValue, TComparer, TAllocator>& cont)
+	template<typename TArchive, typename TMapKey, typename TValue, typename THasher, typename TKeyEq, typename TAllocator>
+	void SerializeArray(TArchive& arrayScope, std::unordered_multimap<TMapKey, TValue, THasher, TKeyEq, TAllocator>& cont)
 	{
 		Detail::SerializeMultiMapImpl(arrayScope, cont);
 	}
